b1d293a75302efda9f1099b75b53982da6cd0d34
Golovnya Vitaliy <vitas210295@gmail.com>
2022-03-28 19:55:37 +1000

    Corrections done

--- main.py
+++ main.py
@@ -7,31 +7,25 @@ def with_log(path):
             x = old_function(*args, **kwargs)
             with open(path + r'\log.txt', 'w', encoding='utf-8') as file:
                 file.write(f'{datetime.now()} >> {old_function.__name__} >> {args, kwargs} >> {x}')
+            return x
+
         return new_function
+
     return _with_log
 
 
-@with_log(r'C:\Users\vital\Desktop\Python projects\AdPy No.5')
-def read_cookbook (f="cookbook.txt", mode="rt"):
-    cook_book = {}
-    with open (f, mode, encoding="utf-8") as file:
-        while True:
-            dish = file.readline().strip()
-            if not dish:
-                break
-            cook_book[dish] = []
-            ingredients = dict()
-            for i in range(int(file.readline())):
-                lst = file.readline().strip().split(" | ")
-                ingredients["ingredient_name"] = lst[0]
-                ingredients["quantity"] = int(lst[1])
-                ingredients["measure"] = lst[2]
-                cook_book[dish] += [ingredients.copy()]
-            file.readline()
-    return cook_book
+path = r'C:\Users\vital\Desktop\Python projects\AdPy No.5'
 
 
-read_cookbook()
+@with_log(path)
+def summator(x, y):
+    return x + y
 
 
+three = summator(1, 2)
+five = summator(2, 3)
 
+result = summator(three, five)
+
+print('result: ', result)
+print('result type: ', type(result))
